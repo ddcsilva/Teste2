49cee4e4d1932477ed056058288c754d4b2cbac9
Danilo Silva <danilo.silva@msn.com>
2025-05-27 16:08:09 -0300

    Refactor routing to include MainLayoutComponent and restructure SuportePlataforma route as a child; update redirection logic.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'cadastrosBasicos/suportePlataforma',
-    loadComponent: () =>
-      import(
-        './features/suporte-plataforma/pages/suporte-plataforma.component'
-      ).then((m) => m.SuportePlataformaComponent),
-  },
-  {
     path: '',
-    redirectTo: '/cadastrosBasicos/suportePlataforma',
-    pathMatch: 'full',
+    loadComponent: () =>
+      import('./core/layout/main-layout/main-layout.component').then(
+        (m) => m.MainLayoutComponent
+      ),
+    children: [
+      {
+        path: 'cadastrosBasicos/suportePlataforma',
+        loadComponent: () =>
+          import(
+            './features/suporte-plataforma/pages/suporte-plataforma.component'
+          ).then((m) => m.SuportePlataformaComponent),
+      },
+      {
+        path: '',
+        redirectTo: '/cadastrosBasicos/suportePlataforma',
+        pathMatch: 'full',
+      },
+    ],
   },
 ];
